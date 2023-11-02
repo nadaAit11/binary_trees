32a20d4057272900790112c3d39fc00eb2e90c74
nada <nadaait171@gmail.com>
2023-11-02 16:39:11 -0700

    array to avl

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -1,21 +1,33 @@
 #include "binary_trees.h"
 
 /**
-* array_to_avl - creates an AVL tree from an array.
-* @array: pointer to the first element of the array to be converted.
-* @size:  number of element in the array.
-* Return: pointer to the root node of the created AVL tree, or NULL if failed
-*/
+ * array_to_avl - Builds an AVL tree from an array.
+ * @array: A pointer to the first element of the array to be converted.
+ * @size: The number of elements in @array.
+ *
+ * Return: A pointer to the root node of the created AVL, or NULL upon failure.
+ */
 avl_t *array_to_avl(int *array, size_t size)
 {
-	size_t i;
 	avl_t *tree = NULL;
+	size_t i, j;
 
-	if (!array)
+	if (array == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		avl_insert(&tree, array[i]);
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (array[j] == array[i])
+				break;
+		}
+		if (j == i)
+		{
+			if (avl_insert(&tree, array[i]) == NULL)
+				return (NULL);
+		}
+	}
 
 	return (tree);
 }
