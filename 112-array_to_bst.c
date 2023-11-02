801e95f846182ce30269d4c5a3069da885fe2386
NEAZYIT <touihnazih28@gmail.com>
2023-11-02 11:18:02 -0700

    Implement array_to_bst function for Binary Search Tree (BST) creation

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,38 +1,41 @@
 #include "binary_trees.h"
 
 /**
- * array_to_bst - Builds a Binary Search Tree (BST) from an array.
+ * array_to_bst - Builds a binary search tree from an array.
  * @array: A pointer to the first element of the array to be converted.
- * @size: The number of elements in the array.
+ * @size: The number of elements in @array.
  *
- * Description: This function constructs a Binary Search Tree (BST)
- * from an array of integers.
- * If a value is already present in the tree, it will be ignored.
- *
- * Return: A pointer to the root node of the created BST, or NULL on failure.
+ * Return: A pointer to the root node of the created BST, or NULL upon failure.
  */
 
 bst_t *array_to_bst(int *array, size_t size)
 {
-	bst_t *root = NULL;
-	size_t index;
+	bst_t *tree = NULL;
+	size_t i, j;
 
-	/* If array is NULL or empty, return NULL */
-	if (array == NULL || size == 0)
+	/* If array is NULL, return NULL */
+	if (array == NULL)
 		return (NULL);
 
 	/* Iterate through each element in the array */
-	for (index = 0; index < size; index++)
+	for (i = 0; i < size; i++)
 	{
-		/* Insert element into BST */
-		if (bst_insert(&root, array[index]) == NULL)
+		/* Check if current element already exists in the array */
+		for (j = 0; j < i; j++)
 		{
-			/* If insertion fails */
-			/* return NULL without freeing the BST */
-			return (NULL);
+			if (array[j] == array[i])
+				break;
+		}
+
+		/* If current element doesn't exist in the array */
+		if (j == i)
+		{
+			/* Insert element into BST */
+			if (bst_insert(&tree, array[i]) == NULL)
+				return (NULL);
 		}
 	}
 
 	/* Return root of BST */
-	return (root);
+	return (tree);
 }
