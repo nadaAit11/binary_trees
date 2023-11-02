12a4cf3400f88173c3250d0e4c7e9e2aa576c42e
nada <nadaait171@gmail.com>
2023-11-02 14:18:43 -0700

    updates

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,18 +9,9 @@
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-	{
 		return (0);
-	}
-
 	if (tree->left == NULL && tree->right == NULL)
-	{
-		/* If both left and right child are NULL, it's a leaf */
 		return (1);
-	}
-	/* Recursively count leaves in left and right subtrees */
-	size_t left_leaves = binary_tree_leaves(tree->left);
-	size_t right_leaves = binary_tree_leaves(tree->right);
-	/* Total leaves in the entire tree */
-	return (left_leaves + right_leaves);
+	return (binary_tree_leaves(tree->right) +
+	binary_tree_leaves(tree->left));
 }
